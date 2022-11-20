aad78a83a32130f042f1045e0cf6e9d3a0aebfc5
JonSeven <mazahaker5555@yandex.ru>
2022-11-20 23:55:03 +0300

    Начальная фиксация

---
+++ Main.java
@@ -0,0 +1,9 @@
+import core.*;
+import tasks.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ну погнали!");
+    }
+}
---
+++ tasks/Subtask.java
@@ -0,0 +1,4 @@
+package tasks;
+
+class Subtask extends Task {
+}

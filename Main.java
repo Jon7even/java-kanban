f2c25c8073a3bbe7b9c6a8a7137a0212c426e167
JonSeven <mazahaker5555@yandex.ru>
2022-11-20 23:56:41 +0300

    Начальная фиксация

--- Main.java
+++
@@ -1,9 +0,0 @@
-import core.*;
-import tasks.*;
-
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Ну погнали!");
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
-public class Main {
+import core.*;
+import tasks.*;
 
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        System.out.println("Ну погнали!");
     }
 }

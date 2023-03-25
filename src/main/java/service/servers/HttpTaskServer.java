e879933e47570f6d1de784ced78acb4d86000aec
JonSeven <mazahaker5555@yandex.ru>
2023-03-25 20:06:34 +0300

    feat: добавлена обработка приоритетных задач

--- src/main/java/service/servers/HttpTaskServer.java
+++ src/main/java/service/servers/HttpTaskServer.java
@@ -45,20 +45,19 @@ public class HttpTaskServer {
         try (h){
             final String path = h.getRequestURI().getPath().substring(7);
             switch (path) {
-                case "task" -> handleTask(h);
-                default -> {
-                    String mError = "Запрашиваемый адрес <b>" + "http://localhost:" + PORT + h.getRequestURI()
-                            + "</b> не существует. "
-                        + "Доступные страницы: <ul>"
-                        + "<a href=\"\\tasks\\\"><li>Priority Tasks</a></li>"
-                        + "<a href=\"\\tasks\\task\"><li>ALL Tasks</a></li>"
-                        + "<a href=\"\\tasks\\subtask\"><li>ALL Subtasks</a></li>"
-                        + "<a href=\"\\tasks\\epic\"><li>ALL Epic</a></li>"
-                        + "<a href=\"\\tasks\\history\"><li>History</a></li>"
-                        + "</ul>";
-                    sendServerMassage("*Клиент пытался зайти на несуществующую страницу: " + h.getRequestURI());
-                    sendResponse(h, mError, 404);
+                case "" -> {
+                    if (h.getRequestMethod().equals("GET")) {
+                        sendServerMassage("Клиент сделал запрос на получение приоритетных задач");
+                        final TreeSet<Task> pTasks = fileTaskManager.getPrioritizedTasks();
+                        final String response = gson.toJson(pTasks);
+                        sendResponse(h, response, 200);
+                        sendServerMassage("Успешно обработан запрос на получение приоритетных задач");
+                    } else {
+                        handleError(h, "requestMethodG");
+                    }
                 }
+                case "task" -> handleTask(h);
+                default -> handleError(h, "endpoint");
             }
         } catch (Exception e) {
             throw new HttpTaskServerException("Error: ", e);
@@ -87,19 +86,52 @@ public class HttpTaskServer {
                 sendResponse(h,response, 200);
                 sendServerMassage("Успешно обработан запрос на получение задачи с ID=" + id);
             }
-            default -> {
-                String mError = "Приложение <b>Task Manager</b> на текущий момент не поддерживает метод - <b>"
-                        + h.getRequestMethod() + "</b> <br />"
+            default -> handleError(h, "requestMethodGPD");
+        }
+    }
+
+    private void handleError(HttpExchange h, String method) throws IOException {
+        String mError = "";
+        switch (method) {
+            case "requestMethodG" -> {
+                mError = "Приложение <b>Task Manager</b> на текущий момент не поддерживает метод - <b>"
+                        + h.getRequestMethod() + "</b> на этой странице. <br />"
+                        + "Доступные методы: <ul>"
+                        + "<li><b>GET</b></li>"
+                        + "</ul>";
+                sendServerMassage("*Клиент пытался использовать необработанный метод " + h.getRequestMethod()
+                        + " на странице: " + h.getRequestURI());
+            }
+            case "requestMethodGPD" -> {
+                mError = "Приложение <b>Task Manager</b> на текущий момент не поддерживает метод - <b>"
+                        + h.getRequestMethod() + "</b> на этой странице. <br />"
                         + "Доступные методы: <ul>"
                         + "<li><b>GET</b></li>"
                         + "<li><b>POST</b></li>"
                         + "<li><b>DELETE</b></li>"
                         + "</ul>";
                 sendServerMassage("*Клиент пытался использовать необработанный метод " + h.getRequestMethod()
-                    + " на странице: " + h.getRequestURI());
-                sendResponse(h, mError, 404);
+                        + " на странице: " + h.getRequestURI());
+            }
+
+            case "endpoint" -> {
+                mError = "Запрашиваемый адрес <b>" + "http://localhost:" + PORT + h.getRequestURI()
+                        + "</b> не существует. "
+                        + "Доступные страницы: <ul>"
+                        + "<a href=\"\\tasks\\\"><li>Priority Tasks</a></li>"
+                        + "<a href=\"\\tasks\\task\"><li>ALL Tasks</a></li>"
+                        + "<a href=\"\\tasks\\subtask\"><li>ALL Subtasks</a></li>"
+                        + "<a href=\"\\tasks\\epic\"><li>ALL Epic</a></li>"
+                        + "<a href=\"\\tasks\\history\"><li>History</a></li>"
+                        + "</ul>";
+                sendServerMassage("*Клиент пытался зайти на несуществующую страницу: " + h.getRequestURI());
+            }
+            default -> {
+                mError = "Произошла непредсказуемая ошибка. Свяжитесь пожалуйста с администратором сервера";
+                sendServerMassage("*Произошла неизвестная ошибка: " + h.getRequestURI());
             }
         }
+        sendResponse(h, mError, 404);
     }
 
     protected void sendResponse(HttpExchange h, String text, int rCode) throws IOException {
